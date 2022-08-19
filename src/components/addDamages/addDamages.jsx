f2e2d2bb48f62d14d5583c490d3e83976fc1a5d8
andreysinus <79992614510@ya.ru>
2022-08-19 09:56:23 +0300

    add car

--- src/components/addDamages/addDamages.jsx
+++ src/components/addDamages/addDamages.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import carViews from '../../img/carAllViews.svg'
+import './addDamages.scss'
 
 function AddDamages() {
   return (
-    <div>addDamages</div>
+    <div>
+      <div className='carviews'>
+        <img src={carViews} alt="Стороны авто" />
+      </div>
+    </div>
   )
 }
 
