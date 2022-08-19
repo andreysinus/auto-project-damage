e896b79cca8c8511d940d05b0cf8c244bcbdcc76
andreysinus <79992614510@ya.ru>
2022-08-19 10:15:02 +0300

    fix

---
+++ src/components/bigButton/bigButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './bigButton.scss'
+import '../../img/bigButton.svg'
+
+function BigButton(props) {
+  return (
+    <div>bigButton</div>
+  )
+}
+
+export default BigButton
