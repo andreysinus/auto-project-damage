2483094a90ad8b75be7c059d793dbfdfaf5ff7db
andreysinus <79992614510@ya.ru>
2022-08-19 15:31:17 +0300

    scroll in block add damage

--- src/components/choosingIssues/choosingIssues.jsx
+++ src/components/choosingIssues/choosingIssues.jsx
@@ -9,6 +9,10 @@ function ChoosingIssues(props) {
         </div>
         <h1 className='choosingissue__title'>{props.selectedPart.name}</h1>
       </div>
+      <div className='choosingissue__body'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti illo similique sint amet et fugit debitis officiis delectus dolorem, necessitatibus dolores fugiat? Eveniet quis non, blanditiis cupiditate hic, rerum similique temporibus accusamus delectus ratione, illo itaque sed ex aperiam officia quibusdam labore ipsa ullam iusto? Incidunt cumque inventore minima quis reiciendis possimus assumenda, dolor molestias culpa blanditiis, eius aut qui? Laudantium nobis saepe recusandae ipsum eligendi laboriosam odit mollitia culpa porro? Officiis accusamus esse aut recusandae dolorum ut vel sequi voluptatibus, atque est a consequatur molestias illum harum dolorem deserunt provident odio facere sunt repudiandae error. Molestias aliquid deserunt sunt ex suscipit fugit eveniet ipsam voluptatum tempora hic quibusdam error enim velit placeat vitae nihil veritatis ipsum cupiditate cum sint unde, eaque repellat quam! Minima dicta voluptatibus perspiciatis deserunt maxime, magni omnis accusantium. Dolor minus eaque mollitia vel, dolores quam, provident ab voluptatem sed non dolorem magni accusamus incidunt error?</div>
+      <div className='choosingissue__footer'>
+        <button className='choosingissue__addbutton'>Добавить повреждения</button>
+      </div>
     </div>
   )
 }
