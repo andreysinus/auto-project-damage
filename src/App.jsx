d47632a2d4a38538421196218602f79f815fdce9
andreysinus <79992614510@ya.ru>
2022-09-07 15:31:57 +0300

    updated for debug

--- src/App.jsx
+++ src/App.jsx
@@ -82,6 +82,9 @@ function App() {
   }
   return (
     <div className='container'>
+      {queryParams.grz}
+      <br/>
+      {queryParams.telephone}
       <div className="logo__anim"><Logo /></div>
       <SelectMenu  selectState={selectMenuState} setSelectState={setSelectMenuState} bucketLength={bucketState.length}/>
 
