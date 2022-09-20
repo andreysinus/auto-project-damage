9ef69694028f17aa9212c90f58101960caf33dac
andreysinus <79992614510@ya.ru>
2022-09-20 10:27:24 +0300

    fixed

--- src/components/missingParts/missingParts.jsx
+++ src/components/missingParts/missingParts.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 import './missingParts.scss'
-import { Scrollbars } from 'react-custom-scrollbars-2';
 
 function MissingParts(props) {
     let partsArr = props.Array
@@ -22,21 +21,18 @@ function MissingParts(props) {
         <h1 className="missing__title">
             Выберите отсутствующие вещи
         </h1>
-        <Scrollbars
-                style={{ height: 376 }}>
-            <div className="missing__body">
-                {props.parts.map((text, index)=>{
-                    return <div key={index} className="missing__item">
-                        <div className="missing__item-check">
-                            <input className="missing__checkbox" type="checkbox"  checked={partsArr[index]!==undefined?true:false} onChange={()=>{console.log(index); handleChangeSelect(index, text.name, text.price); forceUpdate()}}/>
-                        </div>
-                        <div className="missing__item-body">
-                            <p className="missing__item-body-title">{text.name}</p>
-                        </div>
+        <div className="missing__body">
+            {props.parts.map((text, index)=>{
+                return <div key={index} className="missing__item">
+                    <div className="missing__item-check">
+                        <input className="missing__checkbox" type="checkbox"  checked={partsArr[index]!==undefined?true:false} onChange={()=>{console.log(index); handleChangeSelect(index, text.name, text.price); forceUpdate()}}/>
                     </div>
-                })}
-            </div>
-        </Scrollbars>
+                    <div className="missing__item-body">
+                        <p className="missing__item-body-title">{text.name}</p>
+                    </div>
+                </div>
+            })}
+        </div>
         <div className="missing__footer">
             <button className="missing__footer-button" onClick={()=>{props.setAddProgressState("1"); props.setArray(partsArr)}}>Добавить предметы</button>
         </div>
